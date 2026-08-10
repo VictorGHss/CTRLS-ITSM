1561b1a81d44398f3d677c2cc0ec8f49c14fc16c
VictorGHss <vg.hss0101@gmail.com>
2026-08-10 11:34:21 -0300

    feat(review): atualiza nome do template de avaliacao para pesquisa_avaliacao_google_itsm_v6

--- api/src/main/java/br/dev/ctrls/inovareti/modules/appointment/application/service/SendPostAppointmentReviewUseCase.java
+++ api/src/main/java/br/dev/ctrls/inovareti/modules/appointment/application/service/SendPostAppointmentReviewUseCase.java
@@ -28,7 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 @RequiredArgsConstructor
 public class SendPostAppointmentReviewUseCase {
 
-    private static final String TEMPLATE_REVIEW_GOOGLE = "pesquisa_avaliacao_google_itsm_v5";
+    private static final String TEMPLATE_REVIEW_GOOGLE = "pesquisa_avaliacao_google_itsm_v6";
     private static final int FEEGOW_STATUS_ATENDIDO = 3;
 
     private final AppointmentExternalPort appointmentExternalPort;
