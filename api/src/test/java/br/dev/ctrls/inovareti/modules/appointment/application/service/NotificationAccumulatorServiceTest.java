5f74b30964337867fd25a8da9cd66c2ab47183dd
VictorGHss <vg.hss0101@gmail.com>
2026-08-27 11:21:36 -0300

    test(appointment): remove import nao utilizado em NotificationAccumulatorServiceTest

--- api/src/test/java/br/dev/ctrls/inovareti/modules/appointment/application/service/NotificationAccumulatorServiceTest.java
+++ api/src/test/java/br/dev/ctrls/inovareti/modules/appointment/application/service/NotificationAccumulatorServiceTest.java
@@ -1,6 +1,5 @@
 package br.dev.ctrls.inovareti.modules.appointment.application.service;
 
-import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
